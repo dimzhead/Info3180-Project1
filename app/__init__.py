0e60e3d2b64d033d869bd2b78af80a27bda01377
dimzhead <dimzhead@gmail.com>
2020-04-15 18:52:06 -0500

    changed uri and profile

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 SECRET_KEY = "change this to be a more random key"
-SQLALCHEMY_DATABASE_URI = "postgresql://uniejdwejpajrc:2c62823acbe7ffa053b0488996592b3498be54505159bc74bfd6142c8bcad636@ec2-18-206-84-251.compute-1.amazonaws.com:5432/d2rhu69ms8mi14/user_profile"
+SQLALCHEMY_DATABASE_URI = "postgresql://uniejdwejpajrc:2c62823acbe7ffa053b0488996592b3498be54505159bc74bfd6142c8bcad636@ec2-18-206-84-251.compute-1.amazonaws.com:5432/d2rhu69ms8mi14"
 SQLALCHEMY_TRACK_MODIFICATIONS = True # added just to suppress a warning
 UPLOAD_FOLDER = './app/static/uploads'
 
